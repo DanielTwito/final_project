141b7609f9608ac9b048b88ec511e41d331e4d45
DanielTwito <daniel.twito7@gmail.com>
2020-05-10 18:40:19 +0300

    adding add report forn

--- platforms/browser/www/pages/home/homeCtrl.js
+++ platforms/browser/www/pages/home/homeCtrl.js
@@ -8,32 +8,27 @@ app.controller('homeCont', function($rootScope,$scope,$http,$location) {
        // / $scope.show_form = false;
         $rootScope.logUser = localStorage.getItem("logged_user");
         $rootScope.logged_flag=true;
+
         $location.path('/home');
 
     };
 
-    $scope.test=function(){
-        var image = document.getElementById('myImage');
-        // image.src = "data:image/jpeg;base64," + imageData;
-        image.src =  "img/person_1.jpg";
-        image.style.height="400px";
-        image.style.width="300px";
+    $scope.test=function() {
+        $rootScope.currentReport.imgURI = "img/person_1.jpg";
+        image.style.height = "400px";
+        image.style.width = "300px";
+        $rootScope.currentReport.class = $scope.getClassification();
+        //get location of the given photo
 
-        var url=encodeURI(SERVER_URL +"/model_predict");
-        var options = new FileUploadOptions();
-        options.fileKey = "file"; //depends on the api
-        options.fileName = image.src.substr(image.src.lastIndexOf('/')+1);
-        options.mimeType = "multipart/form-data";
-        options.chunkedMode = false; //this is important to send both data and files
+        $scope.getPosition();
 
-        options.headers = {'Content-Type':'application/json'};
-        var ft = new FileTransfer();
-        ft.upload(image.src, url, (res)=> {
-            alert("OK");
-
-        }, (err)=>{
-            alert(err.exception)}, options);
-
+        setTimeout(() => {
+            // $rootScope.currentReport.show  =  true;
+            $rootScope.currentReport.lan = $scope.a;
+            $rootScope.currentReport.lon = $scope.b;
+            document.getElementById("addform").style.display = 'block';
+            // alert(JSON.stringify($rootScope.currentReport));
+        }, 3000);
 
 
     };
@@ -41,7 +36,7 @@ app.controller('homeCont', function($rootScope,$scope,$http,$location) {
     //this function take a photo from camera ( work only in phonegap)
     $scope.cameraTakePicture = function() {
         navigator.camera.getPicture(onSuccess, onFail, {
-            quality: 50,
+            quality: 75,
             destinationType: Camera.DestinationType.FILE_URI,
             encodingType: Camera.EncodingType.JPEG,
             sourceType: Camera.PictureSourceType.CAMERA,
@@ -50,12 +45,16 @@ app.controller('homeCont', function($rootScope,$scope,$http,$location) {
 
         function onSuccess(imageData) {
             var image = document.getElementById('myImage');
-            image.src = "data:image/jpeg;base64," + imageData;
+            // image.src = "data:image/jpeg;base64," + imageData;
             image.src =  imageData;
+            image.style.height="400px";
+            image.style.width="300px";
             $rootScope.currentReport.imgURI = imageData;
             // alert(imageData);
             image.style.height="400px";
             image.style.width="300px";
+            document.getElementById("photo_flag").style.display='none';
+            $rootScope.currentReport.class =  $scope.getClassification();
 
             //get location of the given photo
             $scope.getPosition();
@@ -76,11 +75,11 @@ app.controller('homeCont', function($rootScope,$scope,$http,$location) {
 
 
     $scope.cameraTakePicture_fromGallery = function() {
-
         navigator.camera.getPicture(onSuccess, onFail, {
-            quality: 50,
+            quality: 75,
             destinationType: Camera.DestinationType.FILE_URI,
             sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            encodingType: Camera.EncodingType.JPEG,
             correctOrientation:true
         });
 
@@ -90,12 +89,13 @@ app.controller('homeCont', function($rootScope,$scope,$http,$location) {
             $rootScope.currentReport.imgURI = imageData;
             image.style.height="400px";
             image.style.width="300px";
+            document.getElementById("photo_flag").style.display='none';
+            $rootScope.currentReport.class =  $scope.getClassification();
             //get location of the given photo
 
             $scope.getPosition();
 
             setTimeout(()=>{
-                // $rootScope.currentReport.show  =  true;
                 $rootScope.currentReport.lan  =  $scope.a ;
                 $rootScope.currentReport.lon  =  $scope.b ;
                 document.getElementById("addform").style.display='block';
@@ -108,7 +108,27 @@ app.controller('homeCont', function($rootScope,$scope,$http,$location) {
             alert('Failed because: ' + message);
         }
     };
+    $scope.getClassification = function(){
+        let imageData = $rootScope.currentReport.imgURI;
+        var url = encodeURI(SERVER_URL +"/model_predict");
+        var options = new FileUploadOptions();
+        options.fileKey = "file"; //depends on the api
+        options.fileName = imageData.substr(imageData.lastIndexOf('/')+1);
+        options.mimeType = "image/jpeg";
+        options.chunkedMode = false;
+        options.headers = {
+            'authrization': localStorage.getItem('token')
+        };
+        var ft = new FileTransfer();
+        // ft.upload(imageData, url, (res)=> alert(JSON.stringify(res.response)), (err)=>{alert(JSON.stringify(err))}, options);
+        var params = {};
+        options.params = params;
+        ft.upload( imageData, url, (res)=> {
+            $rootScope.currentReport.class =JSON.parse(res.response).predictions;
+            $scope.init_class_selection();
+        }, (err)=>{alert(JSON.stringify(err))}, options);
 
+    };
     $scope.getPosition = function(){
         navigator.geolocation.getCurrentPosition(onSuccess, onError, {
             enableHighAccuracy: true,
@@ -133,6 +153,7 @@ app.controller('homeCont', function($rootScope,$scope,$http,$location) {
             alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
         }
     };
+
     $scope.one =function(index) {
         if(index === 1 )
             $scope.cameraTakePicture();
@@ -153,31 +174,72 @@ app.controller('homeCont', function($rootScope,$scope,$http,$location) {
     $scope.addReport = function () {
         let photo_disc = $scope.photo_description;
         let imageData = $rootScope.currentReport.imgURI;
-        var url = encodeURI(SERVER_URL +"/model_predict");
+        var url = encodeURI(SERVER_URL +"/addReport");
         var options = new FileUploadOptions();
+        // imageData = "/img/person_2.jpg";
+
         options.fileKey = "file"; //depends on the api
         options.fileName = imageData.substr(imageData.lastIndexOf('/')+1);
         options.mimeType = "image/jpeg";
         options.headers = {
-            'authrization': localStorage.getItem('token'),
-            "latitude":1,
-            "longitude":2
+            'authrization': localStorage.getItem('token')
         };
         // options.chunkedMode = false; //this is important to send both data and files
         var ft = new FileTransfer();
         // ft.upload(imageData, url, (res)=> alert(JSON.stringify(res.response)), (err)=>{alert(JSON.stringify(err))}, options);
         var params = {};
-        params.latitude = 1;
-        params.longitude = 2;
+        // params.latitude = $scope.lan;
+        params.latitude = 10;
+        // params.longitude = $scope.lon;
+        params.longitude = 55;
+        params.date = Date.now();
 
         options.params = params;
+        options.chunkedMode = false;
 
         // url=encodeURI(SERVER_URL +"/addReport");
-        ft.upload( imageData, url, (res)=> alert(JSON.stringify(res.response)), (err)=>{alert(JSON.stringify(err))}, options);
-        alert($scope.photo_description);
+        ft.upload( imageData, url, (res)=>{
+            alert(JSON.stringify(res))
+        }, (err)=>{alert(JSON.stringify(err))}, options);
+        // alert($scope.photo_description);
 
 
     };
+    $scope.init_class_selection = function(){
+        $rootScope.currentReport.choosen_class = $rootScope.currentReport.class[0];
+        // alert($rootScope.currentReport.class);
+        let x = $rootScope.currentReport.class;
+        document.getElementById("class1").style.background = "rgba(0,0,0,0.2)";
+        document.getElementById("class1").value = x[0]+"";
+        document.getElementById("class2").style.background = "white";
+        document.getElementById("class2").value = x[1]+"";
+        document.getElementById("class3").style.background = "white";
+        document.getElementById("class3").value= x[2]+"";
+    };
+    $scope.click1 = function(){
+        $rootScope.currentReport.choosen_class = $scope.class1;
+        document.getElementById("class2").style.background = "white";
+        document.getElementById("class1").style.background = "rgba(0,0,0,0.2)";
+        document.getElementById("class3").style.background = "white";
+    };
+
+
+    $scope.click2=function(){
+        $rootScope.currentReport.choosen_class = $scope.class2;
+        document.getElementById("class1").style.background = "white";
+        document.getElementById("class3").style.background = "white";
+        document.getElementById("class2").style.background = "rgba(0,0,0,0.2)";
+    };
+
+
+    $scope.click3=function(){
+        $rootScope.currentReport.choosen_class = $scope.class3;
+        document.getElementById("class1").style.background = "white";
+        document.getElementById("class2").style.background = "white";
+        document.getElementById("class3").style.background = "rgba(0,0,0,0.2)";
+    };
+
+
 
 
 
